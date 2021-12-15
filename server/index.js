7e1da7a9cd04f92c91b424d519fc91509adc51e4
Iain Cattermole <iain_c@live.co.uk>
2021-12-15 16:37:52 +0000

    Create node server

---
+++ server/index.js
@@ -0,0 +1,77 @@
+/**
+ * Before you can run this example you will have to install a dependency:
+ *
+ *     npm install socket.io
+ */
+ var app = require('http').createServer(handler);
+ var io = require('socket.io')(app, {
+     cors: {
+         origin: "*",
+         methods: ["GET", "POST"]
+     }
+ });
+ var fs = require('fs');
+ var path = require('path');
+ 
+ var PORT = 8081;
+ 
+ app.listen(PORT);
+ 
+ 
+ console.log('Server listening at http://localhost:' + PORT);
+ 
+ function handler(req, res) {
+     // Set CORS headers
+     const headers = {
+         'Access-Control-Allow-Origin': req.headers.origin,
+         'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+         'Access-Control-Max-Age': 2592000, // 30 days
+         /** add other headers as per requirement */
+     };
+ 
+     console.log('request', req.url);
+     
+     if (req.method === 'OPTIONS') {
+         res.writeHead(204, headers);
+         res.end();
+         return;
+     }
+ 
+     
+ 
+     if (req.url === '/timesync/timesync.js') {
+         headers['Content-Type'] = 'application/javascript';
+         res.writeHead(200, headers);
+         return sendFile(path.join(__dirname, '../../../dist/timesync.js'), res, headers);
+     }
+ 
+     if (req.url === '/' || req.url === 'index.html') {
+         return sendFile(__dirname + '/index.html', res);
+     }
+ 
+     res.writeHead(404, headers);
+     res.end('Not found');
+ }
+ 
+ io.on('connection', function (socket) {
+     socket.on('timesync', function (data) {
+         console.log('message', data);
+         socket.emit('timesync', {
+             id: data && 'id' in data ? data.id : null,
+             result: Date.now()
+         });
+     });
+ });
+ 
+ function sendFile(filename, res, headers) {
+     fs.readFile(filename,
+         function (err, data) {
+             if (err) {
+                 res.writeHead(500, headers);
+                 return res.end('Error loading ' + filename.split('/').pop());
+             }
+ 
+             res.writeHead(200, headers);
+             res.end(data);
+         });
+ }
