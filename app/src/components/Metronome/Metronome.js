9822d562b8c67f6a10228ba099234c737e096325
Iain Cattermole <iain_c@live.co.uk>
2021-12-17 16:20:21 +0000

    Add sounds

--- app/src/components/Metronome/Metronome.js
+++ app/src/components/Metronome/Metronome.js
@@ -1,21 +1,37 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import useInterval from '../../utils/useInterval';
 import './Metronome.css';
 
+const clickSounds = [
+  new Audio('/sounds/click.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+  new Audio('/sounds/smallclick.mp3'),
+];
 
 const Metronome = ({ offset, timeSignature, bpm }) => {
-  const [time, setTime] = useState(Date.now() + offset);
+  const [number, setNumber] = useState(0);
 
   useInterval(() => {
-    setTime(Date.now() + offset);
-  }, 1000 / 60);
+    const time = Date.now() + offset;
+    const [beatsPerMeasure, denominator] = timeSignature;
 
-  const [beatsPerMeasure, denominator] = timeSignature;
+    const millisPerBeat = 60000 / (bpm * denominator / 4);
+    const millisPerBar = millisPerBeat * beatsPerMeasure
+  
+    const newNumber = Math.ceil((time % millisPerBar) / millisPerBeat);
+    if (newNumber !== number) {
+      setNumber(newNumber);
+      click(newNumber);
+    }
+  }, 10);
 
-  const millisPerBeat = 60000 / (bpm * denominator / 4);
-  const millisPerBar = millisPerBeat * beatsPerMeasure
-
-  const number = Math.ceil((time % millisPerBar) / millisPerBeat);
+  const click = useCallback((number) => {
+    clickSounds[number - 1].play();
+  }, []);
 
   return  (
     <div>
