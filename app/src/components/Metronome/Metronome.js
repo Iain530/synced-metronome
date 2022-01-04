fb46bf4168d121016cb05050a161d21e6cd01e16
Iain Cattermole <iain_c@live.co.uk>
2022-01-04 11:34:30 +0000

    Remove click, add letters

--- app/src/components/Metronome/Metronome.js
+++ app/src/components/Metronome/Metronome.js
@@ -2,40 +2,39 @@ import { useCallback, useState } from 'react';
 import useInterval from '../../utils/useInterval';
 import './Metronome.css';
 
-const clickSounds = [
-  new Audio('/sounds/click.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-  new Audio('/sounds/smallclick.mp3'),
-];
+const toLetter = n => (n % 26 + 10).toString(36).toUpperCase();
 
 const Metronome = ({ offset, timeSignature, bpm }) => {
   const [number, setNumber] = useState(0);
+  const [letterIndex, setLetterIndex] = useState(0);
 
   useInterval(() => {
     const time = Date.now() + offset;
     const [beatsPerMeasure, denominator] = timeSignature;
 
     const millisPerBeat = 60000 / (bpm * denominator / 4);
-    const millisPerBar = millisPerBeat * beatsPerMeasure
-  
+    const millisPerBar = millisPerBeat * beatsPerMeasure;
+
+    const newLetter = Math.ceil((time % (millisPerBar * 26)) / millisPerBar);
     const newNumber = Math.ceil((time % millisPerBar) / millisPerBeat);
     if (newNumber !== number) {
       setNumber(newNumber);
-      click(newNumber);
+      setLetterIndex(newLetter);
     }
   }, 10);
 
-  const click = useCallback((number) => {
-    clickSounds[number - 1].play();
-  }, []);
-
-  return  (
+  return (
     <div>
-      { number }
+      <div className="letters">
+        {
+          [...Array(4).keys()].map(i => (
+            <div>{ toLetter(letterIndex + i) }</div>
+          ))
+        }
+      </div>
+      <div>
+        { number }
+      </div>
     </div>
   );
 };
